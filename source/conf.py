93f9ebac8b70f76c0ad33e32cd81e15870e579b7
Rune Flobakk <rune.flobakk@posten.no>
2023-08-29 09:48:41 +0200

    Fix language tag for Sphinx autogenerated content
    Fixes warning:
    WARNING: Invalid configuration value found: 'language = None'. Update your configuration to a valid language code. Falling back to 'en' (English).

--- source/conf.py
+++ source/conf.py
@@ -77,7 +77,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -204,4 +204,3 @@ epub_exclude_files = ['search.html']
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
-
